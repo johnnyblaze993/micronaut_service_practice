4dc23433ad7ad17a15cbd899d5e5ba17ef0436ce
Jonathan Alva <john.alvarez.93@gmail.com>
2023-05-04 13:50:28 -0400

    added the functionality to call the backend but getting errors with the fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+	const [name, setName] = useState("");
+	const [greeting, setGreeting] = useState("");
+
+	async function sayHello() {
+		const response = await fetch(`http://localhost:8080/hello/${name}`, {
+			headers: {
+				"User-Agent": "navigtor.userAgent",
+				"content-type": "application/json",
+			},
+		});
+		const data = await response.text();
+		setGreeting(data);
+	}
+
 	return (
 		<div className="App">
 			<h1>Micronaut service headers test</h1>
+			<input
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+				placeholder="Enter your name or else!"
+				style={{
+					width: "300px",
+				}}
+			/>
+			<button onClick={sayHello}>Submit Name</button>
+			<p>{greeting}</p>
 		</div>
 	);
 }
