9944d22e7aad9573b17ece781e6e6206276a3425
Jonathan Alva <john.alvarez.93@gmail.com>
2023-05-04 13:08:19 -0400

    created hello route and a get request that returns users name

--- server/src/main/java/server/helloWorldController.java
+++ server/src/main/java/server/helloWorldController.java
@@ -1,5 +1,16 @@
 package server;
 
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.annotation.Header;
+
+@Controller("/hello")
 public class helloWorldController {
+
+    @Get("/{name}")
+    public String hello(String name, @Header(HttpHeaders.USER_AGENT) String userAgent) {
+        return "Hello " + name + " your user agent is: " + userAgent;
+    }
 
 }
